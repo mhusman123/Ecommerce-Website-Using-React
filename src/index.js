159498d9510fce85e98423c02fe06cf5fb90b9ae
Muhammad Usman <musmanmahar5312@gmail.com>
2025-10-17 18:07:41 +0500

    chore: clean meta links and FA import; docs: README dark-mode updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/theme.css";
